e8398fcb0b36bf38c8829900941a270e955a62e8
RajnishPuri <rajnishpuri15@gmail.com>
2023-12-15 18:32:46 +0530

    Backend

--- models/Transport.js
+++ models/Transport.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const schema=mongoose.Schema;
 
+
 const TransportSchema=new schema({
     farmerId:{type:String,required:true},
     name:{type:String,required:true},
@@ -10,7 +11,9 @@ const TransportSchema=new schema({
     quantity:{type:String,required:true},
     sourceAddress:{type:String,required:true},
     destAddress:{type:String,required:true},
-    vehicleType:{type:String,required:true}
+    vehicleType:{type:String,required:true},
+    // i want to add a key progress which store in format progress:{initiated:true,shipped:false, "out for delivery":false , delivered:false}  }
+    progress:{type:Object,required:true,default:{initiated:true,shipped:false, outForDelivery :false , delivered:false} },
 });
 
 module.exports=mongoose.model('Transport',TransportSchema);
