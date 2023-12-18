277cbae95639d6823c0d2e50b859d52458fef6ec
Krishna Raj <78550375+krishna-raz@users.noreply.github.com>
2023-12-18 16:52:25 +0530

    b2w

--- models/userregister.js
+++ models/userregister.js
@@ -11,10 +11,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
       },
-      passwordAgain: {
-        type: String,
-        required: true
-      },
+     
       name: {
         type: String,
         required: true
@@ -35,6 +32,51 @@ const userSchema = new mongoose.Schema({
      
     });
     
+
+
+    // Middleware to verify token
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(' ')[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+
+
+
+// Middleware to verify token and check user role
+function verifyTokenAndRole(role) {
+  return function (req, res, next) {
+    // Get token from headers or wherever it's stored
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(403).json({ message: 'Token not provided' });
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+
+      // Check user role
+      if (decoded.role !== role) {
+        return res.status(403).json({ message: 'Unauthorized access' });
+      }
+
+      // Attach the decoded user information to the request for later use if needed
+      req.user = decoded;
+
+      // If role matches, proceed to the next middleware or route handler
+      next();
+    });
+  };
+}
 
     
 
